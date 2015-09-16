d02bee2598eac7e19fd392ce833a5a909ac6b4a5
Travis Elliott <travis@controlanything.com>
2015-09-16 11:13:05 -0500

    Added long polling for checking device's online status.

--- www/js/index.js
+++ www/js/index.js
@@ -1,36 +1,21 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
-
-// console.log=function(m){
-// var $=jQuery;
-// var log=$('#console');
-// if(log.length==0){
-// var log=$('<div></div>');
-// log.attr('id', 'console');
-// log.css({position:'fixed', bottom:0, width:'100%', height:'30%', overflow:'auto', background:'white'});
-// $('body').append(log);
-// }
-// var message_wrapper=$('<div class="message"></div>');
-// message_wrapper.text(JSON.stringify(m));
-// log.append(message_wrapper);
-// log.scrollTo(log.innerHeight());
-// }
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 function popup() {
 	var $ = jQuery;
@@ -54,6 +39,7 @@ function popup() {
 
 (function($) {
 	$(document).ready(function() {
+		//Send Post request to Particle server to obtain an access token on the user's behalf.
 		$.post("https://api.particle.io/oauth/token", {
 			client_id : 'particle',
 			client_secret : 'particle',
@@ -63,55 +49,65 @@ function popup() {
 		}, function() {
 			console.log("Post request sent");
 		}).success(function(data) {
+			//Got access token for accessing user info on Particle server
 			$('#statusLabel').text("Getting Device List");
 			accessToken = data.access_token;
-			console.log(accessToken);
 			var requestURL = 'https://api.particle.io/v1/devices?access_token=' + accessToken;
-
+			//Send get request to get a list of user's devices
 			$.get(requestURL, function() {
 			}).done(function(devices) {
 				$('#statusLabel').text("Device List Loaded");
-				console.log(devices);
+				//For Each loop to load each device's information
 				$.each(devices, function() {
 					var device = this;
 					var name = device.name == null ? "Unnamed Device" : device.name;
 					var li = $("<li></li>");
-					li.text(name).appendTo("#deviceList");
-					if(device.connected == true){
-						li.addClass("connected").click(function(){
-							$('#listOfDevices').css({display:"none"});
-							$('#deviceView').css({display:"block"});
+					//Add device to list view
+					li.text(name).appendTo("#deviceList").attr("id", device.id);
+					if (device.connected == true) {
+						li.addClass("connected").click(function() {
+							$('#listOfDevices').css({
+								display : "none"
+							});
+							$('#deviceView').css({
+								display : "block"
+							});
 							$('#deviceNameHeader').text(device.name);
-							var deviceInfoURL = "https://api.particle.io/v1/devices/"+device.id+"?access_token="+accessToken;
-							$.get(deviceInfoURL, function(){
-								
-							}).done(function(deviceInfo){
-								$.each(deviceInfo.functions, function(){
+							var deviceInfoURL = "https://api.particle.io/v1/devices/" + device.id + "?access_token=" + accessToken;
+							$.get(deviceInfoURL, function() {
+
+							}).done(function(deviceInfo) {
+								$.each(deviceInfo.functions, function() {
 									var deviceFunction = this;
 									var functionLI = $("<li></li>");
-									functionLI.text(deviceFunction).appendTo('#deviceFunctionList').click(function(){
+									functionLI.text(deviceFunction).appendTo('#deviceFunctionList').click(function() {
 										var userInput = prompt("Enter function Argument");
-										if(userInput){
-											var functionURL = "https://api.particle.io/v1/devices/"+device.id+"/"+deviceFunction;
-											$.post(functionURL,{
-												arg:userInput,
-												access_token:accessToken
-											}, function(){
-												
-											}).success(function(data){
+										if (userInput) {
+											var functionURL = "https://api.particle.io/v1/devices/" + device.id + "/" + deviceFunction;
+											$.post(functionURL, {
+												arg : userInput,
+												access_token : accessToken
+											}, function() {
+
+											}).success(function(data) {
 												console.log(data);
 											});
 										}
 									});
 								});
-							}).fail(function(){
+							}).fail(function() {
 								console.log("Failed to load device info");
 							});
 						});
+					} else {
+						//Device not connected so we really dont need to do anything but display it to the user.
 					}
 
 				});
 
+				window.setInterval(function() {
+					reloadDevices(requestURL);
+				}, 2000);
 				console.log(devices);
 			}).fail(function() {
 				$('#statusLabel').text("Error loading Device List");
@@ -125,6 +121,30 @@ function popup() {
 			$('#statusLabel').text("Error Connecting to Server");
 		});
 	});
+
+	function reloadDevices(url) {
+		$.get(url, function() {
+		}).done(function(devices) {
+			$('#statusLabel').text("Device List Loaded");
+			//For Each loop to load each device's information
+			$.each(devices, function() {
+				var device = this;
+				if (device.connected == true) {
+					$('#'+device.id).addClass('connected');
+				} else {
+					//Device not connected so we really dont need to do anything but display it to the user.
+					$('#'+device.id).removeClass('connected');
+				}
+
+			});
+		}).fail(function() {
+			$('#statusLabel').text("Error loading Device List");
+		});
+		$.get(url, function(data) {
+
+		});
+	}
+
 })(jQuery);
 
 var app = {
@@ -161,3 +181,9 @@ var app = {
 		console.log('Received Event: ' + id);
 	}
 };
+
+(function($) {
+	$(document).on("mobileinit", function() {
+		console.log("jQuery Mobile init");
+	});
+})(jQuery);
